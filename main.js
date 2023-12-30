54873d70a91375ae36c4460a2e177570a7a15281
Amna Yousafzai <yousafza@mcmaster.ca>
2023-12-29 22:26:04 -0500

    UI completed

--- main.js
+++ main.js
@@ -1,57 +1,71 @@
+const buttons = document.querySelectorAll('button');
+let player_score=0, computer_score=0;
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        playerSelection= button.id;
+        document.getElementById('p3').innerHTML= button.id;
+        computerSelection= getComputerChoice();
+        document.getElementById('p4').innerHTML= computerSelection;
+        console.log(playerSelection);
+        playRound(playerSelection, computerSelection);
+    })
+})
+
+
 const choices= ["rock","paper","scissors"];
 function getComputerChoice(){
     let randInt = Math.floor(Math.random()*choices.length); //Math.floor rounds down to the nearest integer and math.random generates a floating point number between 0 (inclusive) and 1 (exclusive)
     return choices[randInt];
 }
 
-function getPlayerInput(){
-    let validInput = false;
-    while(validInput==false){
-        const choice= prompt("Enter rock, paper or scissor: ");
-        if(choice == null){
-            continue;
-        }
-        const choiceInLower = choice.toLowerCase();
-        console.log(choiceInLower);
-        if(choices.includes(choiceInLower)){
-            validInput = true;
-            return choiceInLower;
-        }
-    }
-}
 
 function playRound(playerSelection, computerSelection){
-    if(playerSelection==computerSelection.toLowerCase()){
-        return "Tie"
+    while(player_score!=5 && computer_score!=5){
+        result="";
+        if(playerSelection==computerSelection.toLowerCase()){
+            result= "Tie";
+        }
+        else if((playerSelection=="rock" && computerSelection == "scissors") || (playerSelection=="paper" && computerSelection == "rock") || (playerSelection=="scissors" && computerSelection == "paper")) {
+            result= "You win";
+            player_score+=1;
+        }
+        else{
+            result= "You lose";
+            computer_score+=1;
+        }
+        document.getElementById('result').innerHTML= result;
+        document.getElementById('p_score').innerHTML= "Score:"+ player_score;
+        document.getElementById('c_score').innerHTML= "Score:"+computer_score;
+        return
     }
-    else if((playerSelection=="rock" && computerSelection == "Scissors") || (playerSelection.toLowerCase()=="paper" && computerSelection == "Rock") || (playerSelection.toLowerCase()=="scissors" && computerSelection == "Paper")) {
-        return "Player"
+    if(player_score>computer_score)
+    {
+        alert("You win")
+    }
+    else if(computer_score>player_score)
+    {
+        alert("You lose")
     }
     else{
-        return "Computer"
+        alert("Tie")
     }
+    
 }
 
 function game(){
-    var player_score=0, computer_score=0;
-    for(let i=0; i<5;i++)
-    {
-        const computerSelection= getComputerChoice();
-        console.log(computerSelection)
-        
-        
-        if(playRound(getPlayerInput(), computerSelection)=="Player"){
-            player_score+=1;
-            console.log("player gets 1 point")
-        }
-        else if(playRound(getPlayerInput(), computerSelection)=="Computer"){
-            computer_score+=1;
-            console.log("comp gets one point")
-        }
-        else{
-            console.log('tie')
-        }
+    
+    if(playRound(getPlayerInput(), computerSelection)=="Player"){
+        player_score+=1;
+        console.log("player gets 1 point")
     }
+    else if(playRound(getPlayerInput(), computerSelection)=="Computer"){
+        computer_score+=1;
+        console.log("comp gets one point")
+    }
+    else{
+        console.log('tie')
+    }
+    
     console.log(computer_score);
     console.log(player_score);
 
@@ -68,4 +82,4 @@ function game(){
     }
 }
 
-console.log(game())
+
